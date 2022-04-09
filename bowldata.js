ede11d3052054392e722b626c2726729ff6c5f37
cn27529 <cn27529@hotmail.com>
2022-04-09 13:38:59 +0800

    bowl v1.0 w3.js

---
+++ bowldata.js
@@ -0,0 +1,16 @@
+{
+  "myMenus": [{
+      "name": "index",
+      "value": "./index.html"
+    },
+    {
+      "name": "aboutbowl",
+      "value": "#aboutbowl"
+    },
+    {
+      "name": "contact",
+      "value": "#contact"
+    }
+  ]
+
+}
